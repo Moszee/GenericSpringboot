ed263db9a26e2d3c8625b4b2f4d991468b979eb9
mszpak <moszee@gmail.com>
2016-08-31 00:38:44 +0200

    initial commit

---
+++ src/main/java/org/szpax/GenericSpringbootApplication.java
@@ -0,0 +1,12 @@
+package org.szpax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericSpringbootApplication.class, args);
+	}
+}
